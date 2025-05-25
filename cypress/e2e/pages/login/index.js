1ae458fa0c520c66a404b1e13d6f6ee508edb901
Douglas Souza <douglas45308@gmail.com>
2025-05-25 11:48:13 -0300

    Refatory files

--- cypress/e2e/pages/login/index.js
+++ cypress/e2e/pages/login/index.js
@@ -15,8 +15,32 @@ class Login{
         cy.get(el.password).type('senha123');
         cy.get(el.cadastrarButton).click();
         cy.contains('Cadastro realizado com sucesso').should('be.visible');
-        cy.screenshot('deve cadastrar um novo usuário via interface');
+        cy.log('Usuário cadastrado com SUCESSO !');
+        cy.screenshot('Usuário cadastrado com sucesso');
+        
 
+    }
+
+    cadastrarNovoUsuarioAdministrador(){
+        cy.get(el.nome).type('Administrador Teste');
+        cy.get(el.email).type(`admin${Date.now()}@teste.com`);
+        cy.get(el.password).type('senha123');
+        cy.get(el.administrador).check();
+        cy.get(el.cadastrarButton).click();
+        cy.contains('Cadastro realizado com sucesso').should('be.visible');
+        cy.log('Usuário administrador cadastrado com SUCESSO ! ');
+        cy.screenshot('Usuário administrador cadastrado com sucesso');
+        
+    }
+
+    validarMensagemCampoObrigatorio(){
+        cy.get(el.cadastrarButton).click();
+        cy.contains('Nome é obrigatório').should('be.visible');
+        cy.contains('Email é obrigatório').should('be.visible');
+        cy.contains('Password é obrigatório').should('be.visible');
+        cy.log('Validação de campos obrigatórios realizada com SUCESSO !');
+        cy.screenshot('Validação de campos obrigatórios realizada com sucesso');
+        
     }
 
    
--- cypress/e2e/web/usuario-web.cy.js
+++ cypress/e2e/web/usuario-web.cy.js
@@ -7,26 +7,19 @@ describe('Usuário Web', () => {
        Login.acessarPagina();
     })
 
-    it.only('deve cadastrar um novo usuário via interface', () => {
+    it('deve cadastrar um novo usuário via interface', () => {
         Login.cadastrarNovoUsuario();
-     
+        
+        
     });
   
     it('deve cadastrar um novo usuário administrador via interface', () => {
-      cy.get('input[name="nome"]').type('Administrador Teste');
-      cy.get('input[name="email"]').type(`admin${Date.now()}@teste.com`);
-      cy.get('input[name="password"]').type('senha123');
-      cy.get('input[name="administrador"]').check();
-      cy.get('button[type="submit"]').click();
-      cy.contains('Cadastro realizado com sucesso').should('be.visible');
-      cy.screenshot('deve cadastrar um novo usuário administrador via interface');
+        Login.cadastrarNovoUsuarioAdministrador();
+        
     });
   
     it('deve validar mensagens de erro para campos obrigatórios', () => {
-      cy.get('button[type="submit"]').click();
-      cy.contains('Nome é obrigatório').should('be.visible');
-      cy.contains('Email é obrigatório').should('be.visible');
-      cy.contains('Password é obrigatório').should('be.visible');
-      cy.screenshot('deve validar mensagens de erro para campos obrigatórios');
+        Login.validarMensagemCampoObrigatorio();
+     
     });
   });
